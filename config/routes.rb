08bdc305892b3aabdac6cb05f7927b0d9678c6df
takeoka <tokih4ru@gmail.com>
2020-08-11 15:29:15 +0900

    ユーザー編集機能作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'pictures#top'
   resources :sessions, only: [:new, :create, :destroy]
-  resources :users, only: [:new, :create, :show]
+  resources :users, only: [:new, :create, :show, :edit, :update]
   resources :pictures
 end
